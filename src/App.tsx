fef6520b332e44d81cc14e75cfcb6725676b67da
Louis Chenais <lchenais@gmail.com>
2024-10-23 10:28:22 +0200

    Add Stack component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './assets/styles/style.scss'
 import Text from './components/01-atoms/Text/Text'
+import Stack from './components/01-atoms/Stack/Stack'
 import Button from './components/02-molecules/Button/Button'
 
 function App() {
@@ -8,13 +9,17 @@ function App() {
 
   return (
     <>
-      <Text tag='h1' variant='title-1'>Couou</Text>
-      <Button onClick={() => setCount((count) => count + 1)}>
-        <Text>{`count is ${count}`}</Text>
-      </Button>
-      <Button variant='secondary' onClick={() => setCount((count) => count + 1)}>
-        <Text>{`count is ${count}`}</Text>
-      </Button>
+      <Stack gap={4}>
+        <Text tag='h1' variant='title-1'>Couou</Text>
+        <Stack gap={20}>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            <Text>{`count is ${count}`}</Text>
+          </Button>
+          <Button variant='secondary' onClick={() => setCount((count) => count + 1)}>
+            <Text>{`count is ${count}`}</Text>
+          </Button>
+        </Stack>
+      </Stack>
     </>
   )
 }
---
+++ src/components/01-atoms/Stack/Stack.tsx
@@ -0,0 +1,31 @@
+import { ElementType, ReactNode } from "react";
+import './Stack.scss';
+
+interface StackProps {
+  as?: ElementType;
+  axis?: 'x' | 'y';
+  gap?:
+  1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 12
+  | 14
+  | 16
+  | 18
+  | 20;
+  children: ReactNode;
+}
+
+function Stack({ children, axis = 'y', gap = 2, as = 'div' }: StackProps) {
+  const Tag = as;
+  return (<Tag className={`a-stack ${axis === 'y' ? 'a-stack--vertical' : ''}`} style={{ '--space': `var(--base-space-${gap})` }}>{children}</Tag>)
+}
+
+export default Stack;
