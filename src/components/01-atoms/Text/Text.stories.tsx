46f344b4b698fd5dc5a72cb94cd865a6562516ba
Louis Chenais <lchenais@gmail.com>
2024-10-30 14:19:43 +0100

    Clean Text stories file

--- src/components/01-atoms/Text/Text.stories.tsx
+++ src/components/01-atoms/Text/Text.stories.tsx
@@ -2,20 +2,13 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import Text from "./Text";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "01-atoms/Text",
   component: Text,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  // argTypes: {
-  //   variant: { control: 'color' },
-  // },
   args: {
     children: ["The quick brown fox jumped over the lazy frog"],
   },
