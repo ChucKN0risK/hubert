dd3a85b3e02f55189124a30e6f12936bc277b5ea
Louis Chenais <lchenais@gmail.com>
2024-11-04 18:00:05 +0100

    Update Accordion component

--- src/components/02-molecules/Accordion/Accordion.tsx
+++ src/components/02-molecules/Accordion/Accordion.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import Text from '../../01-atoms/Text/Text'
 import Icon from '../../01-atoms/Icon/Icon'
 import Stack from '../../01-atoms/Stack/Stack'
+import type { TextVariant } from '../../01-atoms/Text/Text'
 import './Accordion.scss'
 
 // See: https://stackoverflow.com/a/73050903/3906770
@@ -9,6 +10,7 @@ interface FolderProps extends React.ComponentPropsWithoutRef<'details'> {
   summary: string;
   children: ReactNode | ReactNode[];
   summaryIcon?: string;
+  summaryTextVariant?: TextVariant;
   /**
    * Is the Accordion open by default
    * @defaultValue false
@@ -16,12 +18,12 @@ interface FolderProps extends React.ComponentPropsWithoutRef<'details'> {
   open?: boolean;
 }
 
-function Folder({ summary, summaryIcon, open = false, children, ...props }: FolderProps) {
+function Folder({ summary, summaryIcon, summaryTextVariant, open = false, children, ...props }: FolderProps) {
   return (
     <details className='m-accordion' open={open} aria-expanded={open} {...props}>
       <Stack axis="x" as='summary' align='center'>
         {summaryIcon ? <Icon name={summaryIcon} className='m-accordion__summary-icon' /> : null}
-        <Text>{summary}</Text>
+        <Text variant={summaryTextVariant}>{summary}</Text>
         <Icon name='caret-right' className='m-accordion__toggle-icon' />
       </Stack>
       <div className="m-accordion__content">
