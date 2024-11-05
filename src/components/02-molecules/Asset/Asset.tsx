3032d190a6c7998224f91abf8e6f83c05cc9d4f3
Louis Chenais <lchenais@gmail.com>
2024-11-05 17:27:36 +0100

    Fix video size

--- src/components/02-molecules/Asset/Asset.tsx
+++ src/components/02-molecules/Asset/Asset.tsx
@@ -14,17 +14,31 @@ function Folder({ asset, onSelection }: FolderProps) {
   };
 
   // Check if the file is an image based on its extension
-  const isImage = asset.name.match(/\.(jpg|jpeg|png|gif|webp|svg)$/i);
+  const isImage = asset.name.match(/\.(jpg|jpeg|png|gif|avif|webp|svg)$/i);
+  const isVideo = asset.name.match(/\.(mp4)$/i);
+  const isPDF = asset.name.match(/\.(pdf)$/i);
 
-  if (!isImage) {
-    return <div>{asset.name} is not an image file.</div>;
+  if (isImage) {
+    return (
+      <button className='m-asset' onClick={handleClick}>
+        <img src={`http://localhost:3000/assets/${asset.path}`} alt='' />
+      </button>
+    )
+  }
+  if (isVideo) {
+    return (
+      <video src={`http://localhost:3000/assets/${asset.path}`}></video>
+    )
   }
 
-  return (
-    <button className='m-asset' onClick={handleClick}>
-      <img src={`http://localhost:3000/assets/${asset.path}`} alt='' />
-    </button>
-  )
+  // if (isPDF) {
+  //   return (
+  //     <iframe src={`http://localhost:3000/assets/${asset.path}`} width="100%" height="100%">
+  //       {/* <a href="home.pdf">Download the PDF</a> */}
+  //     </iframe>
+  //   )
+  // }
+
 }
 
 export default Folder
