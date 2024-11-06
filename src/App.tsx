1d76156c62c33beae611be5f6ff41216d5545a58
Louis Chenais <lchenais@gmail.com>
2024-11-06 11:20:21 +0100

    Add label to searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-// import { useState } from 'react'
-import data from '../backend/data.json';
+import data from '../api/data.json';
 import './assets/styles/style.scss';
 import Text from './components/01-atoms/Text/Text';
 import Input from './components/02-molecules/Input/Input';
 import Sidebar from './components/03-organisms/Sidebar/Sidebar';
+import AssetList from './components/02-molecules/AssetList/AssetList';
+import AssetInfo from './components/02-molecules/AssetInfo/AssetInfo';
 import FolderList from './components/03-organisms/FolderList/FolderList';
-import AssetList from './components/03-organisms/AssetList/AssetList';
 import { FolderProvider } from './hooks/useFolder';
+import { AssetProvider } from './hooks/useAsset';
 import { ThemeProvider } from './hooks/useTheme';
-import ThemeSwitcher from './components/02-molecules/ThemeSwitcher/ThemeSwitcher';
+// import ThemeSwitcher from './components/02-molecules/ThemeSwitcher/ThemeSwitcher';
 
 function App() {
   const handleChange = (e) => {
@@ -19,20 +20,25 @@ function App() {
     <>
       <ThemeProvider>
         <FolderProvider>
-          <Sidebar>
-            <Text variant='body-3'>Folders</Text>
-            <FolderList folders={data.children} />
-          </Sidebar>
-          <main>
-            <ThemeSwitcher />
-            <header>
-              <Input icon='search' placeholder='Search' onChange={() => handleChange} />
-            </header>
-            <AssetList />
-          </main>
-          <Sidebar>
-            Coucou
-          </Sidebar>
+          <AssetProvider>
+            <Sidebar>
+              <div className="search">
+                <label htmlFor="search" className='u-visually-hidden'></label>
+                <Input id='search' icon='search' placeholder='Search' onChange={() => handleChange} />
+              </div>
+              <Text>Folders</Text>
+              <FolderList folders={data.children} />
+            </Sidebar>
+            <main>
+              {/* <header>
+                <ThemeSwitcher />
+              </header> */}
+              <AssetList />
+            </main>
+            <Sidebar>
+              <AssetInfo />
+            </Sidebar>
+          </AssetProvider>
         </FolderProvider>
       </ThemeProvider>
     </>
