2ab5d9ee5c429e0bbd14cc37cd88991716c31bd7
Louis Chenais <lchenais@gmail.com>
2024-11-12 16:32:56 +0100

    Update Storybook config

---
+++ .storybook/preview.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Preview } from '@storybook/react';
+import "../src/assets/styles/style.scss";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div className="storybook">
+        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default preview;
