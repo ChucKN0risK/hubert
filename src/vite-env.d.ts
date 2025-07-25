c4da85221ba4c322c9c92e17f992592a1a274b61
Louis Chenais <lchenais@gmail.com>
2025-07-25 15:54:10 +0200

    Add File System Access API type definitions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="vite/client" />
+
+interface Window {
+  showDirectoryPicker(): Promise<FileSystemDirectoryHandle>;
+}
+
+interface FileSystemDirectoryHandle {
+  kind: "directory";
+  name: string;
+  entries(): AsyncIterableIterator<[string, FileSystemHandle]>;
+}
+
+interface FileSystemFileHandle {
+  kind: "file";
+  name: string;
+  getFile(): Promise<File>;
+}
+
+type FileSystemHandle = FileSystemFileHandle | FileSystemDirectoryHandle;
